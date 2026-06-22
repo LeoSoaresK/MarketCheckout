030bc7a9859d0eee17124dabceea324fc7040389
LeoSoaresK <leosojunior@gmail.com>
2026-06-21 21:43:19 -0300

    calculo de total multiplicando preco por quantidade

--- src/carrinho.py
+++ src/carrinho.py
@@ -1,2 +1,5 @@
 def calcular_total(carrinho):
-    return 0.0
+    total = 0.0
+    for item in carrinho:
+        total += item["preco"] * item["quantidade"]
+    return total
--- tests/test_carrinho.py
+++ tests/test_carrinho.py
@@ -4,3 +4,10 @@ def test_calcular_total_carrinho_vazio():
     carrinho = []
     resultado = calcular_total(carrinho)
     assert resultado == 0.0
+
+def test_calcular_total_com_um_item():
+    carrinho = [
+        {"nome": "Maçã", "preco": 2.50, "quantidade": 1}
+    ]
+    resultado = calcular_total(carrinho)
+    assert resultado == 2.50
